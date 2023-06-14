3e2239188d91a4ae5cc73c858a5d5b8b05b5b863
IGo <igor.smirna@gmail.com>
2023-06-14 16:25:06 +0300

    fix: entry css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,39 @@
 import React from "react";
-import { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../images/logo-blanco.svg";
 
-export default function Header({email, loggedIn, isSign}) {
+export default function Header({ email, loggedIn, isSign }) {
   // const inn = 'Войти';
   // const email = 'email@mail.com'
   const currentLocation = useLocation();
-  const [buttonInfo, setButtonInfo] = useState({ path: '', textOfButton: '' });
-  console.log(loggedIn, isSign)
+  const [buttonInfo, setButtonInfo] = useState({ path: "", textOfButton: "" });
+  
+  console.log(loggedIn, isSign);
 
   useEffect(() => {
-    if (currentLocation.pathname === '/sign-in') {
-      setButtonInfo({ path: 'sign-up', textOfButton: 'Зарегистрироваться' });
-    }
-    else if (currentLocation.pathname === '/sign-up') {
-      setButtonInfo({ path: 'sign-in', textOfButton: 'Войти' });
+    if (currentLocation.pathname === "/sign-in") {
+      setButtonInfo({ path: "sign-up", textOfButton: "Зарегистрироваться" });
+    } else if (currentLocation.pathname === "/sign-up") {
+      setButtonInfo({ path: "sign-in", textOfButton: "Войти" });
     }
   }, [currentLocation.pathname]);
 
   return (
     <header className="header">
-      <img
-        src={logo}
-        alt="Логотип Место Россия."
-        className="header__logo"
-      />
-      <div>
-        <article>{loggedIn ? email : ''}</article>
+      <img src={logo} alt="Логотип Место Россия." className="header__logo" />
+      <div className="header__entry">
+        <article>{loggedIn ? email : ""}</article>
         <Link
-                to={loggedIn === false ? buttonInfo.path : '/'}
-                className='header__button'
-                style={loggedIn ? {} : { display: 'block' }}
-                onClick={isSign}
-              >
-                {loggedIn === false ? buttonInfo.textOfButton : 'Выйти'}
-              </Link>
+          to={loggedIn === false ? buttonInfo.path : "/"}
+          className="header__button"
+          style={loggedIn ? {} : { display: "block" }}
+          onClick={isSign}
+        >
+          {loggedIn === false ? buttonInfo.textOfButton : "Выйти"}
+        </Link>
         {/* <button className="header__logged" onClick={isSign}> {loggedIn? 'Выйти':'Войти'}</button> */}
       </div>
-      
     </header>
   );
 }
-
-
